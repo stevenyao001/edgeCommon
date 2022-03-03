e33c9a33a02e0a46a392318b2191e26064b0c955
unknown <dongdong.guo@irootech.com>
2022-03-03 13:02:29 +0800

    updateHttp

--- http/http.go
+++ http/http.go
@@ -19,6 +19,7 @@ type Conf struct {
 	ShutdownTimeout time.Duration
 	Router          func(router *gin.Engine)
 	Wg              *sync.WaitGroup
+	Close           chan struct{}
 }
 
 func InitHttp(conf Conf, middleware ...gin.HandlerFunc) {
@@ -45,16 +46,19 @@ func InitHttp(conf Conf, middleware ...gin.HandlerFunc) {
 	}
 
 	go func() {
-		conf.Wg.Wait()
+		<- conf.Close
 		ctx, cancel := context.WithTimeout(context.Background(), conf.ShutdownTimeout)
 		defer cancel()
 		err = server.Shutdown(ctx)
 		fmt.Println("----- serve shutdown", time.Now().Format("2006-01-02 15:04:05.999999999"), err)
 	}()
 
-	//监听端口
-	err = server.Serve(l)
-	fmt.Println("----- serve close", time.Now().Format("2006-01-02 15:04:05.999999999"), err)
+	go func() {
+		//监听端口
+		err = server.Serve(l)
+		conf.Wg.Done()
+		fmt.Println("----- serve close", time.Now().Format("2006-01-02 15:04:05.999999999"), err)
+	}()
 
 }
 
